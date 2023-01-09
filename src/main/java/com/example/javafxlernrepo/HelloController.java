268c040ecd751162d288a54ace59f5e9e7d40656
David Ebschke <davidebschke8@gmai.com>
2023-01-09 16:19:07 +0100

    add new button und text funktion hinzugefügt

--- src/main/java/com/example/javafxlernrepo/HelloController.java
+++ src/main/java/com/example/javafxlernrepo/HelloController.java
@@ -5,10 +5,14 @@ import javafx.scene.control.Label;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private Label welcomeText,byeText;
 
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
     }
+
+    @FXML
+    protected void onByeButtonClick() {byeText.setText("Bye!");}
+
 }
