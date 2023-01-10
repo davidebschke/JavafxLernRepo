8e45e565d6603973141f73ec6f850d6b06ba57ea
David Ebschke <davidebschke8@gmai.com>
2023-01-10 17:33:11 +0100

    cancel option give a message now and not a failure

--- src/main/java/com/example/javafxlernrepo/CreateFolderController.java
+++ src/main/java/com/example/javafxlernrepo/CreateFolderController.java
@@ -6,6 +6,7 @@ import javafx.stage.DirectoryChooser;
 import javafx.stage.Stage;
 
 import java.io.File;
+import java.util.Objects;
 
 public class CreateFolderController {
 
@@ -21,12 +22,19 @@ public class CreateFolderController {
         String path= ChoosePathForDirectory();
         // aktuell wird der ordner immer mit den Namen examples versehen
 
-        File f = new File(path+"\\Examples");
-        if(f.mkdir()){
-            folderCreate.setText("Folder is Created");
+        if(Objects.equals(path, ""))
+        {
+            folderCreate.setText("Folder is NOT Created, because you have cancel the Directory Choose");
         }
-        else {
-            folderCreate.setText("Folder is NOT Created");
+        else
+        {
+            File f = new File(path+"\\Examples");
+            if(f.mkdir()){
+                folderCreate.setText("Folder is Created");
+            }
+            else {
+                folderCreate.setText("Folder is NOT Created");
+            }
         }
     }
 
@@ -34,7 +42,15 @@ public class CreateFolderController {
 
         String directoryPath;
         File selectedDirectory = directoryChooser.showDialog(primarystage);
-        directoryPath=selectedDirectory.getAbsolutePath();
-        return directoryPath;
+        if(selectedDirectory==null)
+        {
+            directoryPath="";
+            return directoryPath;
+        }
+        else
+        {
+            directoryPath=selectedDirectory.getAbsolutePath();
+            return directoryPath;
+        }
     }
 }
